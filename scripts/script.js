9aef1784080d79235954cd212e830645ced7392c
AD1227 <moths02tatters@icloud.com>
2026-04-15 19:21:32 -0700

    school work- chapter 2
    added greeting using javascript
    added tracker on navigation bar

--- scripts/script.js
+++ scripts/script.js
@@ -52,3 +52,58 @@ links.forEach(function (link) {
     if (target) target.scrollIntoView({ behavior: "smooth" });
   });
 });
+
+// This script is for Greeting
+
+//Variables for the Welcome message
+
+var greeting = "Welcome Back ";
+var name = "Molly";
+var message = "! , Here is your Order Status";
+
+//Concatenate of the three variables to the welcome mesage
+
+var welcome = greeting + name + message;
+
+//Variables that hold details about the sign.
+
+var itemOrdered = [
+  "espresso---------- $9",
+  "Americano--------$9",
+  "Latte-------------- $9",
+];
+
+var subTotal = itemOrdered.length * 9;
+var tip = 20;
+var status = "Ready for Pick up";
+var grandTotal = subTotal + tip;
+
+//Get the element that has an id of greeting
+var el = document.getElementById("greeting");
+el.textContent = welcome;
+
+// get the element that has an if of status
+var elStatus = document.getElementById("status");
+elStatus.textContent = status;
+
+// get the element that has an id for the items
+
+var el_item1 = document.getElementById("item1");
+var el_item2 = document.getElementById("item2");
+var el_item3 = document.getElementById("item3");
+el_item1.textContent = itemOrdered[0];
+el_item2.textContent = itemOrdered[1];
+el_item3.textContent = itemOrdered[2];
+
+//get the element that has an id of subtotal
+
+var elSubTotal = document.getElementById("subTotal");
+elSubTotal.textContent = "$" + subTotal;
+
+//get the element that has an id of tip
+var elTip = document.getElementById("tip");
+elTip.textContent = tip;
+
+//get the element that has an id of grandTotal
+var elGrandTotal = document.getElementById("grandTotal");
+elGrandTotal.textContent = "$" + grandTotal;
